9c6e8f33d633f06f6630017aedbd735f98a4cce1
coolkidgthe3rd-dotcom <coolkidgthe3rd@gmail.com>
2026-05-21 10:05:27 -0400

    Initial commit

--- ConsoleTest/ConsoleTest/ConsoleTest.cpp
+++ ConsoleTest/ConsoleTest/ConsoleTest.cpp
@@ -5,7 +5,11 @@
 
 int main()
 {
+    std::cout << "This message says 1.\n";
+
     std::cout << "This message says 2.\n";
+
+    std::cout << "This message says 3.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
