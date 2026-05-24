724eb8fa18ef53eeedf2a9dfcec16255bd2e2035
coolkidgthe3rd-dotcom <coolkidgthe3rd@gmail.com>
2026-05-24 09:23:24 -0400

    Displaying message

---
+++ Week 01 - Programming Assignment/Week 01 - Programming Assignment/Week 01 - Programming Assignment.cpp
@@ -0,0 +1,9 @@
+// Week 01 - Programming Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
