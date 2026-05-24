6663dbcc2b047a89d0e1f2194f0f90559f6d60b8
coolkidgthe3rd-dotcom <coolkidgthe3rd@gmail.com>
2026-05-24 10:37:49 -0400

    Finalized assignment

--- Week 01 - Programming Assignment/Week 01 - Programming Assignment/Week 01 - Programming Assignment.cpp
+++ Week 01 - Programming Assignment/Week 01 - Programming Assignment/Week 01 - Programming Assignment.cpp
@@ -47,7 +47,7 @@ int main()
     cin >> bodyPart;
 
     //Response to the answer
-    cout << "\nOh, " << bodyPart << " is a great place to start! Me personally, I decided to work on my arms. For almost the entirety of my life,\n"
+    cout << "\nOh, the " << bodyPart << " is a great place to start! Me personally, I decided to work on my arms. For almost the entirety of my life,\n"
         << "my arms have been built like noodles, and so one day I just simply got tired of it, and wanted to make a change.\n"
         << "But I eventually decided I should be workouting out more than simply just my arms.";
 
@@ -63,15 +63,38 @@ int main()
     cout << "What drinks do you like to consume while you workout?\n\n";
 
     //Answer to question three
-    cout << "Type in your response here and press the 'Enter' key to submit your response:\n";
+    cout << "Type in your response here and press the 'Enter' key to submit your response:\n\n";
     cin >> beverage;
 
     //Response to the answer
-    cout << "\nOh sweet, for me personally, it's my arms. For almost the entirety of my life, my arms have been built like noodles, and\n"
-        << "so one day I just simply got tired of it, but I eventually decided I should workout more than just my arms.";
+    cout << "\nAh, " << beverage << " is a classic one. I've always just stuck to water, it's simple, it's usually always the"
+        << "\nleast costly option, you usually can bring it with you, it's healthy, and it always tastes good!";
 
     //Stars because why not
     cout << "\n\n**********************************************************\n\n";
+
+
+
+
+    //Summary of answers
+    cout << "Alright so what I have gathered from our discussion:\n\n";
+
+    cout << "- You like to typically workout for " << workoutLength << " hours per week.\n";
+    cout << "- The main part of your body you want to work on is your " << bodyPart << ".\n";
+    cout << "- You favorite beverage to consume while working out is " << beverage << ".\n";
+
+
+
+    //Words of advice
+    cout << "\n\nThese are all great traits to have for working out!\n";
+    cout << "\nAnd please keep in mind to not get discouraged from seeing other people's progress,";
+    cout << "\neverybody's bodies are different, while some may simply have more time or experience. But whatever";
+    cout << "\nit is, you should never sell yourself short because somebody else is at where you want to be right now,";
+    cout << "\nbecause with enough time, patience, hard work, and dedication, that could be you too.";
+
+
+    //Goodbye
+    cout << "\n\nAnyways, thank you for answering my questions and I wish you the best of luck on your journey!\n\n\n";
 
 
 
