edf766c010bfd7afce2086d644d44333891892b9
coolkidgthe3rd-dotcom <coolkidgthe3rd@gmail.com>
2026-06-07 00:28:21 -0400

    -Added if/else blocks (using compund boolean conditions) -Implemented menu with switch

--- Programming Assignment/Programming Assignment/Programming Assignment.cpp
+++ Programming Assignment/Programming Assignment/Programming Assignment.cpp
@@ -26,6 +26,8 @@ int main()
     string bodyPart;
     string beverage;
     double weight;
+    int option;
+    string singleName;
 
     //Weird variable I made up for a "press to continue feature"
     char um;
@@ -72,22 +74,103 @@ int main()
     //Stars because why not
     cout << "\n\n**********************************************************\n\n";
 
-    //My short & simple introduction
-    cout << "Welcome!! What is your first and last name?\n\n";
+    //Start of program, asks if player has a last name.
+    cout << "Welcome!! Before we begin, we need a name. Do you have a last name? (Yes or No)\n\n";
+    cin >> singleName;
 
-    //To account for the space
-    cin >> firstName;
-    cin.ignore(1);
-    cin >> lastName;
+    //Accounting for variations of 'yes' (if player does have a last name)
+    if (singleName == "yes" || singleName == "Yes" || singleName == "YES")
+    {
+        cout << "\nOkay, please enter your first and last names here:\n\n";
+        //To account for the space
+        cin >> firstName;
+        cin.ignore(1);
+        cin >> lastName;
+    }
+    //Accounting for variations of 'no' (if player does NOT have a last name)
+    else if (singleName == "no" || singleName == "No" || singleName == "NO")
+    {
+        cout << "\nOkay, please enter your name here:\n\n";
+        cin >> firstName;
+    }
+    //If player types anything either than yes or no (terminates program)
+    else
+    {
+        cout << "\nError: Invalid response. Terminating program.\n\n";
+        return 0;
+    }
 
     //Stars because why not
-    cout << "\n\n**********************************************************\n\n";
+    cout << "\n**********************************************************\n\n";
+
+
 
     //My short & simple introduction
-    cout << "Hello " << firstName << " " << lastName << ", my name is Gary and my hobby is strength training & fitness development!\n";
-    cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+    //Accounting for variations of 'yes' (if player does have a last name)
+    if (singleName == "yes" || singleName == "Yes" || singleName == "YES")
+    {
+        //If player shares the same FIRST name as me
+        if (firstName == "Gary" || firstName == "gary")
+        {
+            //If player does NOT share the same FIRST and LAST name as me
+            if (lastName != "Gantt" && lastName != "gantt")
+            {
+                cout << "Woah! Your first name is also " << firstName << "? That's so cool!\n\n";
 
+                cout << "Anyways, moving on, my hobby is strength training & fitness development!\n";
+                cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+            }
 
+            //If player shares BOTH the same first name AND last name as me
+            else if (lastName == "Gantt" || lastName == "gantt")
+            {
+                cout << "Woah this is insane!\n";
+                cout << "Your first AND last name is ALSO " << firstName << " " << lastName << "??\n";
+                cout << "What are the chances?!\n\n";
+                cout << "Wait... are you sure you aren't just a secret clone of me...?\n\n";
+
+                cout << "Anyways, moving on me, our hobby is strength training & fitness development!\n";
+                cout << "I would like to ask you (or myself?) four simple questions related to our hobby.\n\n";
+
+                cout << "(God that is so weird, I'm not doing that anymore...)\n\n";
+            }
+        }
+        //If player shares the same LAST name as me but NOT the first name
+        else if (lastName == "Gantt" || lastName == "gantt")
+        {
+            if (firstName != "Gary" && firstName != "gary")
+            {
+                cout << "Woah! Your last name is also " << lastName << "? That's so cool!\n\n";
+
+                cout << "Anyways, moving on, my hobby is strength training & fitness development!\n";
+                cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+            }
+        }
+        //If player does not share either the first or last name with me
+        else
+        {
+            cout << "Hello " << firstName << " " << lastName << ", my name is Gary and my hobby is strength training & fitness development!\n";
+            cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+        }
+    }
+    //Accounting for variations of 'no' (if player does NOT have a last name)
+    else if (singleName == "no" || singleName == "No" || singleName == "NO")
+    {
+        //If player shares the same first name as me
+        if (firstName == "Gary" || firstName == "gary")
+        {
+            cout << "Woah! Your name is also " << firstName << "? That's so cool!\n\n";
+
+            cout << "Anyways, moving on, my hobby is strength training & fitness development!\n";
+            cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+        }
+        //If player does not share the same first name as me
+        else
+        {
+            cout << "Hello " << firstName << ", my name is Gary and my hobby is strength training & fitness development!\n";
+            cout << "I would like to ask you four simple questions related to my hobby.\n\n";
+        }
+    }
 
 
 
@@ -108,9 +191,15 @@ int main()
     cin.clear();
     cin.ignore(100, '\n');
 
-    //Response to the answer
-    cout << "\nA workout of " << workoutLength << " hours is good! For me, my sessions are still rather short, only really being two hours maximum as\n"
+    //Possible responses to the answer
+    if (workoutLength < 0)
+        cout << "\nWhat? A workout of " << workoutLength << " hours?? How is that possible??\n\nAnyways... moving on...";
+    else if (workoutLength == 0 || workoutLength == 1)
+        cout << "\nAh, a workout of " << workoutLength << " hours is pretty nice, always great to start off small!";
+    else if (workoutLength <= 2)
+        cout << "\nA workout of " << workoutLength << " hours is good! For me, my sessions are still rather short, only really being two hours maximum as\n"
         << "of right now. But that is primarily because I'm still new to this, but I'm aiming to go higher!";
+
 
     //Stars because why not
     cout << "\n\n**********************************************************\n\n";
@@ -179,9 +268,19 @@ int main()
     cin.clear();
     cin.ignore(100, '\n');
 
-    //Response to the answer
-    cout << "\nNice, " << weight << " lbs isn't a bad place to start off.\n"
+    //Possible responses to the answer
+    if (weight <= 60)
+        cout << "\nWhat? You have a weight of " << weight << " pounds?? How is that possible? Do you even exist?\n\nAnyways... moving on...";
+    else if (weight > 60 && weight <= 130)
+        cout << "\nOnly a weight of " << weight << " pounds? You must be pretty slim, so don't worry about burning fat too much!";
+    else if (weight > 130 && weight <= 210)
+        cout << "\nYou already have a " << weight << " pounds? You're already in pretty great shape, kudos to you!";
+    else if (weight > 210 && weight <= 290)
+        cout << "\nNice, " << weight << " pounds isn't a bad place to start off.\n"
         << "I wish you luck on your journey to whatever weight it is that you desire!";
+    else
+        cout << "\nOkay, there's some work to be done here, but that's okay!\n"
+        << "If you put your mind to it, there's no goal that cannot be reached!";
 
     //Stars because why not
     cout << "\n\n**********************************************************\n\n";
@@ -250,8 +349,45 @@ int main()
     cout << "\nbecause with enough time, patience, hard work, and dedication, that could be you too.";
 
 
-    //Goodbye
-    cout << "\n\nAnyways, thank you for answering my questions and I wish you the best of luck on your journey!";
+    //Menu
+    cout << "\n\nBut before you go, please take a moment to choose one of the 3 not so brief tips from this menu:";
+    cout << "\n(Type in a number from 1-3)\n\n";
+
+    cout << "\n1. Guide on eating healthier.";
+    cout << "\n2. Establishing a schedule.";
+    cout << "\n3. Just a random tip.";
+
+    cin >> option;
+
+    switch (option)
+    {
+    case 1:
+        cout << "\n\nOption one selected."
+            << "\n\nOne, don't feel bad about eating three meals, working out a lot can burn a ton"
+            << "\nof energy that you might need. Next, try not to eat too much after 6 if you can"
+            << "\nhelp it, not only does it help eliminate unneeded eating, but it is also great for"
+            << "\nburning weight as it burns out pretty much all the glucose at of you, so it forces"
+            << "\nyour body to run on the actual reserves it has to burn fat off.";
+        break;
+    case 2:
+        cout << "\n\nOption two selected."
+            << "\n\nThis tip is admittedly short compared to tips one and three, but anyways, this"
+            << "\none is such an important, yet neglected and overlooked principal for working out."
+            << "\nWhen working out, don't just train and exercise like a madman, rest is just equally"
+            << "\nas important, if not, more. When you train, TRAIN. When you rest, REST.";
+        break;
+    case 3:
+        cout << "\n\nOption three selected."
+            << "\n\nOkay, so for awhile, I wanted to do more with my forearms, so a few weeks ago,"
+            << "\nI ordered this cool thing I saw on Instagram. It was a black bag that had a ton of"
+            << "\ncool devices to use like arm grip strengtheners, stress balls, etc. It's really"
+            << "\nconvenient because you can use it on the fly, and it's great for forearms!"
+            << "\n\nIt was made by a company called \'FITBEAST\', so I recommend checking that out!";
+        break;
+    default:
+        cout << "\n\nError: Invalid option. Terminating program.";
+        return 0;
+    }
 
     //Stars because why not
     cout << "\n\n**********************************************************\n\n";
